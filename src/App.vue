a27aa6790bbe760ec14ab98c03d9a2ea006efaed
AndreyBS1 <andrey.babenkov.00@gmail.com>
2021-07-30 19:43:51 +0300

    Final version

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,8 @@
   <div id="app">
     <h1>Feed</h1>
     <ul></ul>
-<!--    <ul v-for="(msg, index) in feed"-->
-<!--        :key="index">-->
-<!--      <li>-->
-<!--        <h4>{{ msg.date }} / {{ msg.authorName }} / {{ msg.authorUrl }}</h4>-->
-<!--        <p>{{ msg.content }}</p>-->
-<!--      </li>-->
-<!--    </ul>-->
     <div v-show="loading">
-      <p>Загрузка данных...</p>
+      <p class="loading">Загрузка данных...</p>
     </div>
   </div>
 </template>
@@ -31,11 +24,9 @@ export default {
   methods: {
     getFeed() {
       this.feed = require("./assets/feed.json");
-
       for (let msg of this.feed) {
         msg.date = this.setDate(msg.date);
       }
-
       console.log("\nFeed:\n", this.feed);
     },
 
@@ -44,15 +35,12 @@ export default {
 
       for (let i = this.feedSize - 10; i < this.feedSize; i++) {
         let li = document.createElement("li");
-        let h1 = document.createElement("h1");
         let h4 = document.createElement("h4");
         let p = document.createElement("p");
 
-        h1.innerHTML = `${i}`;
         h4.innerHTML = `${this.feed[i].date} / ${this.feed[i].authorName} / ${this.feed[i].authorUrl}`;
         p.innerHTML = this.feed[i].content;
 
-        li.appendChild(h1);
         li.appendChild(h4);
         li.appendChild(p);
         ul.appendChild(li);
@@ -63,26 +51,18 @@ export default {
 
     contentTone() {
       let nodesList = document.querySelectorAll("p");
-      // console.log("\nNodes list:\n", nodesList);
 
       for (let i = this.feedSize - 10; i < this.feedSize; i++) {
         let node = nodesList[i];
-        // console.log("\nCurrent node:\n", node);
-        // console.log("\nNode's first child:\n", node.firstChild);
 
         for (let item of this.feed[i].contentPostTones) {
-          // console.log("\nStart offset: ", item.position);
-          // console.log("\nEnd offset: ", item.position + item.length);
           let range = new Range();
           range.setStart(node.firstChild, item.position);
           range.setEnd(node.firstChild, item.position + item.length);
           this.ranges.push({range: range, tone: item.tone});
-          // console.log("\nCurrent range:\n", range);
-          // console.log("\nCurrent tone: ", item.tone);
         }
-        // console.log("\nRanges:\n", this.ranges);
+
         for (let item of this.ranges) {
-          // console.log("\nCurrent item of ranges:\n", item);
           try {
             let elem = document.createElement("span");
             elem.style.backgroundColor = this.setColor(item.tone);
@@ -115,7 +95,6 @@ export default {
         let r = Math.round(255 * (1-tone));
         color = `rgb(${r}, 255, 0)`;
       }
-      // console.log("\nCurrent color: ", color);
       return color;
     },
 
@@ -140,8 +119,6 @@ export default {
 
       if (position >= threshold) {
         if (this.loading) return;
-        // console.log("\nCurrent feed size: ",this.feedSize);
-        // console.log("\nFull feed size: ", this.feed.length);
         if (this.feedSize < this.feed.length) {
           this.loading = true;
           await setTimeout(() => {
@@ -188,5 +165,9 @@ ul {
 
 p {
   text-align: start;
+}
+
+.loading {
+  text-align: center;
 }
 </style>
